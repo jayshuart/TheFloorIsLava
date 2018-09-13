49b7b8d7b6e7a972b812b6a8f01e4b2abfea8b6a
pigghead <justin.strongarms10@gmail.com>
2018-09-13 17:31:30 -0400

    merged branches

---
+++ TheFloorIsLava/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerBehavior : MonoBehaviour {
+
+    public Vector3 Position;        // global position of pc
+    public Vector3 Velocity;        // global movement speed of pc
+    public float speedVar;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Position = transform.position;
+        Velocity = new Vector3(0, 0, 0);
+        speedVar = 5.0f;
+	}
+	
+    /// <summary>
+    /// Move player in X/Y axis; allow for diagonal movement
+    /// </summary>
+    void PlayerMovement()
+    {
+        // Track movements in LEFT/RIGHT and FORWARD/BACKWARD
+        var xMovement = Input.GetAxis("Horizontal") * Time.deltaTime * speedVar;
+        var zMovement = Input.GetAxis("Vertical") * Time.deltaTime * speedVar;
+
+        transform.Translate(xMovement, 0, zMovement);
+        transform.Rotate(xMovement, 0, 0);
+    }
+
+    /// <summary>
+    /// Allow for upwards movement; gravity affected
+    /// </summary>
+    void PlayerJump()
+    {
+
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        PlayerMovement();
+	}
+}
