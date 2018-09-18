9921414838742486384f02d16be26ecb891bdcbb
pigghead <justin.strongarms10@gmail.com>
2018-09-18 02:09:52 -0400

    Collision + Player movement updates; Collisions with lava detected (no behavior outside of console.log as of yet; a few options for collision w/ lava) (state manager to handle destruction 'started')
    TODO: Finish camera movement w/ mouse

--- TheFloorIsLava/Assets/Scripts/PlayerBehavior.cs
+++ TheFloorIsLava/Assets/Scripts/PlayerBehavior.cs
@@ -8,8 +8,10 @@ public class PlayerBehavior : MonoBehaviour {
     public Vector3 Position;        // global position of pc
     public Vector3 Velocity;        // global movement speed of pc\
     public Vector3 Heading;         // the rotation/direction the pc is facing
-    public float speedVar;          // 
-    public float turnRate;          //
+    public float speedVar;          // speed of the pc
+    public float turnRate;          // speed in which pc turns
+	public bool isGrounded;			// is the player connected with the ground
+	Rigidbody charRB;				// reference to the PC's rigidbody
 
 	// Use this for initialization
 	void Start ()
@@ -17,6 +19,8 @@ public class PlayerBehavior : MonoBehaviour {
         Position = transform.position;
         Velocity = new Vector3(0, 0, 0);
         speedVar = 5.0f;
+		charRB = GetComponent<Rigidbody> ();
+		isGrounded = false;
 	}
 	
     /// <summary>
@@ -36,17 +40,53 @@ public class PlayerBehavior : MonoBehaviour {
 
     }
 
+	void onGround() 
+	{
+		if (!isGrounded & charRB.velocity.y == 0) {
+			isGrounded = true;
+		}
+	}
+
     /// <summary>
     /// Allow for upwards movement; gravity affected
     /// </summary>
     void PlayerJump()
     {
-
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true) {
+			//Debug.Log ("Jump!");
+			charRB.AddForce (new Vector3 (0, 4.0f, 0), ForceMode.Impulse);
+			isGrounded = false;
+		}
     }
+
+	/// <summary>
+	/// Initial touching of the lava
+	/// </summary>
+	/// <param name="col">Col.</param>
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.collider.name == "PH_Lava") {
+			Debug.Log ("hit");
+		}
+	}
+
+	/// <summary>
+	/// Prolonged touching of the lava
+	/// </summary>
+	/// <param name="col">Col.</param>
+	void OnCollisionStay(Collision col)
+	{
+		if (col.collider.name == "PH_Lava") {
+			Debug.Log ("hit");
+		}
+	}
 
 	// Update is called once per frame
 	void Update ()
     {
         PlayerMovement();
+		onGround ();
+		PlayerJump ();
+		//Debug.Log (isGrounded);
 	}
 }
