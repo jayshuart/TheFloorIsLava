c99545ba68db440f55de60c466d5a647b3ddce6e
Joel Shuart <joelshuart@gmail.com>
2018-09-22 19:04:13 -0400

    replaced NetworkingHUD with basic menu

---
+++ TheFloorIsLava/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MainMenu : MonoBehaviour {
+
+    [SerializeField] private NetworkManager networkManager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        networkManager.StartHost();
+    }
+
+    public void JoinGame()
+    {
+        networkManager.StartClient();
+    }
+}
