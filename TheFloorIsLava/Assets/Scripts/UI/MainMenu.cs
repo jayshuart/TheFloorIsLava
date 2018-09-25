0370546bf3f0bd74f168e3ca01e09cd9bbb89e17
Joel Shuart <joelshuart@gmail.com>
2018-09-25 00:56:43 -0400

    built out submenus for the main menu, and began auto-built level select

--- TheFloorIsLava/Assets/Scripts/UI/MainMenu.cs
+++ TheFloorIsLava/Assets/Scripts/UI/MainMenu.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
 using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
     [SerializeField] private NetworkManager networkManager;
+    [SerializeField] private GameObject levelBtnPrefab;
+    private List<string> sceneList = new List<string>();
 
 	// Use this for initialization
 	void Start () {
@@ -25,5 +30,28 @@ public class MainMenu : MonoBehaviour {
     public void JoinGame()
     {
         networkManager.StartClient();
+    }
+
+    public void BuildLevelSelect()
+    {
+        foreach(EditorBuildSettingsScene level in EditorBuildSettings.scenes)
+        {
+            //get enabled levels that are int he levels folder
+            if (level.enabled && level.path.ToString().Contains("Assets/Scenes/Levels/"))
+            {
+                //add scene to actual list
+                sceneList.Add(level.path);
+
+                //create btn for this level
+                GameObject lvlBtn = Instantiate(levelBtnPrefab, this.gameObject.transform);
+
+                //rip just the levels name from the unity scene file
+                string lvlName = level.path.ToString().Substring(21, (level.path.ToString().Substring(21).Length - 6));
+
+                //apply this name to the buttons text
+                lvlBtn.GetComponentInChildren<Text>().text = lvlName;
+            }
+        }
+
     }
 }
