35b31816ffed114d8bb5dd5124e96b0e300209b9
Joel Shuart <joelshuart@gmail.com>
2018-10-08 16:45:15 -0400

    made lobby prefab with shiny new models, beginings of lobby code

---
+++ TheFloorIsLava/Assets/Scripts/Lobby.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Lobby : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        //check if this is the host
+        if (!col.gameObject.GetComponent<NetworkIdentity>().isServer)
+        {
+            //you have no power here- begone!
+            return;
+        }
+
+        //goto actual level
+        Debug.Log("HOST READY");
+    }
+}
