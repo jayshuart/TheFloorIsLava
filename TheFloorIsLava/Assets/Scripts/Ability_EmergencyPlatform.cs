b30aeef1e09ce38f2cfe8737a043c3d6bbf0f2c1
Joel Shuart <joelshuart@gmail.com>
2018-10-18 21:37:23 -0400

    basic throwing

--- TheFloorIsLava/Assets/Scripts/Ability_EmergencyPlatform.cs
+++ TheFloorIsLava/Assets/Scripts/Ability_EmergencyPlatform.cs
@@ -5,6 +5,9 @@ using UnityEngine.Networking;
 
 public class Ability_EmergencyPlatform : NetworkBehaviour {
 
+    [SerializeField] private GameObject throwablePrefab;
+    [SerializeField] private float throwForce;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,6 +15,26 @@ public class Ability_EmergencyPlatform : NetworkBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (isLocalPlayer)
+        {
+            //left click
+            if (Input.GetMouseButtonDown(0))
+            {
+                //actually throw our emergency platform
+                throwPlatform();
+            }
+        }
 	}
+
+    private void throwPlatform()
+    {
+        //create platform
+        Vector3 instantiatePos = this.transform.position + (this.transform.forward);
+        GameObject throwable = GameObject.Instantiate(throwablePrefab, instantiatePos,  this.transform.localRotation);
+
+        //apply force to it
+        Rigidbody throwRB = throwable.GetComponent<Rigidbody>();
+        Vector3 throwVector = this.transform.forward * throwForce;
+        throwRB.AddForce(throwVector);
+    }
 }
