66f767bf0e03e6f936b9ff0bf02f38c0ad92125b
Joel Shuart <joelshuart@gmail.com>
2018-10-24 23:25:42 -0400

    aoe grow effect

--- TheFloorIsLava/Assets/Scripts/thrownLowGravityAOE.cs
+++ TheFloorIsLava/Assets/Scripts/thrownLowGravityAOE.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class thrownLowGravityAOE : MonoBehaviour {
     [SerializeField] private float upwardForce;
+    [SerializeField] private float scaleRate;
+    private Vector3 fullSize;
 
 	// Use this for initialization
 	void Start () {
-		
+        //save inital scale and then shrink
+        fullSize = this.gameObject.transform.localScale;
+        this.gameObject.transform.localScale = Vector3.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //scale up low grav aoe
+        this.gameObject.transform.localScale = Vector3.Slerp(Vector3.zero, fullSize, scaleRate); 
+        scaleRate += scaleRate * Time.deltaTime; 
 	}
 
     void OnTriggerEnter(Collider col)
@@ -37,5 +43,9 @@ public class thrownLowGravityAOE : MonoBehaviour {
     private void LowGravity(GameObject affectedObject)
     {
         affectedObject.GetComponent<Rigidbody>().AddForce((Vector3.up * upwardForce), ForceMode.Force);
+    }
+
+    private void SLERPAoE(float rate)
+    {
     }
 }
