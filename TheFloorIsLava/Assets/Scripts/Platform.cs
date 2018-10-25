b1269725ba301de52408095a48287b9b93455513
Joel Shuart <joelshuart@gmail.com>
2018-10-24 23:56:30 -0400

    base of sinking

---
+++ TheFloorIsLava/Assets/Scripts/Platform.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Platform : NetworkBehaviour {
+
+    [SerializeField] private Vector3 originalPos;
+    [SerializeField] private float dropDistance;
+    [SerializeField] private float dropRate;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void Sink()
+    {
+        this.transform.position = Vector3.Lerp(originalPos, ((this.transform.forward * -1) * dropDistance), dropRate);
+        dropRate += dropRate * Time.deltaTime;
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Sink();
+        }
+    }
+
+}
