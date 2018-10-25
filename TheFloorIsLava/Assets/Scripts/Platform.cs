ced1d5786790ee955fd232fb291750fadf03dfff
Joel Shuart <joelshuart@gmail.com>
2018-10-25 17:10:13 -0400

    sinking mostly there

--- TheFloorIsLava/Assets/Scripts/Platform.cs
+++ TheFloorIsLava/Assets/Scripts/Platform.cs
@@ -8,27 +8,38 @@ public class Platform : NetworkBehaviour {
     [SerializeField] private Vector3 originalPos;
     [SerializeField] private float dropDistance;
     [SerializeField] private float dropRate;
+    private float distCovered;
 
 	// Use this for initialization
 	void Start () {
-		
+        originalPos = this.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        //check if it needs to be moved back to original pos
+        if (Mathf.Abs(this.gameObject.transform.position.magnitude ) < Mathf.Abs(originalPos.magnitude)) 
+        {
+            Sink(Vector3.up); //up
+        }
 	}
 
-    private void Sink()
+    /// <summary>
+    /// Sink the specified direction
+    /// </summary>
+    /// <param name="direction">Direction.</param>
+    private void Sink(Vector3 direction)
     {
-        this.transform.position = Vector3.Lerp(originalPos, ((this.transform.forward * -1) * dropDistance), dropRate);
-        dropRate += dropRate * Time.deltaTime;
+        distCovered += dropRate * Time.deltaTime;
+        this.transform.position = Vector3.Lerp(originalPos, ((direction * dropDistance) + originalPos), distCovered); //lerp change in pos for smooth movement
     }
 
     void OnCollisionStay(Collision col)
     {
+        //sink down if a player is on it
         if (col.gameObject.CompareTag("Player"))
         {
-            Sink();
+            Sink(Vector3.down); //down
         }
     }
 
