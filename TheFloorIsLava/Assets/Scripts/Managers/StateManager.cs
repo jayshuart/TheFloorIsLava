e7fff7cae12a13eaeee8d5711835c931084e0785
pigghead <justin.strongarms10@gmail.com>
2018-10-25 20:11:01 -0400

    Texture changes/updates; GUI win conditions updated

--- TheFloorIsLava/Assets/Scripts/Managers/StateManager.cs
+++ TheFloorIsLava/Assets/Scripts/Managers/StateManager.cs
@@ -2,30 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class StateManager : NetworkBehaviour {
 
 	// PUBLIC
 	public GameObject playerChar;		// the character
 	public GameObject lavaObj;			// lava object. May need to be an array in the future
+	public Text timeScore;				// time taken to complete level for each player
 
 	// PRIVATE
 	private Rigidbody charRig;			// character's rigidbody
 	private Rigidbody lavaRig;			// laval's rigidbody
 	private GameObject finishLine;		// win state
 	private float elapsedTime;			// total time since start of game
+	//public Text totalScore;				// final time
 
 	// Use this for initialization
 	void Start () {
 		Cursor.visible = false;
 
 		//playerChar = GameObject.FindGameObjectWithTag ("Player");
-
-
-
 		finishLine = GameObject.FindGameObjectWithTag ("Finisher"); // finish line instantiation
 
 		elapsedTime = 0;
+
+		timeScore.color = Color.white;
+
+		//totalScore.transform.SetParent (GameObject.FindGameObjectWithTag (""));
 	}
 
 	void Awake() {
@@ -38,10 +42,11 @@ public class StateManager : NetworkBehaviour {
 	void ReachFinishLine(GameObject other) {
 		Collider tempCollider = other.GetComponent<Collider> ();
 		Collider tempCollider2 = playerChar.GetComponent<Collider> ();
+
 		if (other.tag == "Finisher" && tempCollider2.bounds.Intersects (tempCollider.bounds)) {
 			// display the time taken to reach finish
-			Debug.Log (elapsedTime.ToString ("0.00"));
-			Debug.Log ("Trigger!");
+			///Debug.Log (elapsedTime.ToString ("0.00"));
+			//Debug.Log ("Trigger!");
 		} else {
 			Debug.Log ("Unknown error/exception");
 		}
@@ -64,5 +69,8 @@ public class StateManager : NetworkBehaviour {
 
 		if (playerChar == null) 
 			playerChar = GameObject.FindGameObjectWithTag ("Player");
+
+		timeScore.text = "Time: " + elapsedTime.ToString("0.00");
+
 	}
 }
