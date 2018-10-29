cf5981f2f7ff2621727a424fd2b5eabca3403abf
Joel Shuart <joelshuart@gmail.com>
2018-10-29 00:27:48 -0400

    thrown platforms now sink (dont reemerge though) - still need proper cleanup

--- TheFloorIsLava/Assets/Scripts/thrownPlatform.cs
+++ TheFloorIsLava/Assets/Scripts/thrownPlatform.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class thrownPlatform : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    [SerializeField] private Vector3 originalPos;
+    [SerializeField] private float dropDistance;
+    [SerializeField] private float dropRate;
+    private float distCovered;
+    private bool solid;
+
+    // Use this for initialization
+    void Start () {
+        solid = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
     void OnTriggerEnter(Collider col)
@@ -21,6 +26,29 @@ public class thrownPlatform : MonoBehaviour {
         {
             //freeze object
             this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            originalPos = this.transform.position;
+            solid = true;
         }
     }
+
+    void OnCollisionStay(Collision col)
+    {
+        //sink down if a player is on it
+        if (col.gameObject.CompareTag("Player") && solid)
+        {
+            Sink(Vector3.down); //down
+        }
+    }
+
+    // <summary>
+    /// Sink the specified direction
+    /// </summary>
+    /// <param name="direction">Direction.</param>
+    private void Sink(Vector3 direction)
+    {
+        distCovered += dropRate * Time.deltaTime;
+        this.transform.position = Vector3.Lerp(originalPos, ((direction * dropDistance) + originalPos), distCovered); //lerp change in pos for smooth movement
+    }
+
+   
 }
