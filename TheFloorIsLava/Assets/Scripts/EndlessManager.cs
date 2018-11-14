1623f1fbd60d9f52f020120fb150e4f38a8b7a7e
Joel Shuart <joelshuart@gmail.com>
2018-11-14 18:40:12 -0500

    endless mode now generates new platforms

--- TheFloorIsLava/Assets/Scripts/EndlessManager.cs
+++ TheFloorIsLava/Assets/Scripts/EndlessManager.cs
@@ -30,19 +30,29 @@ public class EndlessManager : MonoBehaviour {
         platformPos = this.gameObject.transform.position; //start from root
         for (int i = 0; i < numOfPlatforms; i++)
         {
-            float gapLeft = maxGap;
+            GameObject block = GeneratePlatform();
 
-            float addZ = Random.Range(0, zVariation); //only go forward
-            gapLeft-= addZ;
-
-            float addX = Mathf.Clamp(Random.Range(-xVariation, xVariation), -gapLeft, gapLeft); //left/right
-            gapLeft -= Mathf.Abs(addX);
-
-
-            float addY = Random.Range(-yVariation, yVariation); //up/down
-
-            platformPos = new Vector3((platformPos.x + addX), (platformPos.y + addY), (platformPos.z + addZ));
-            GameObject.Instantiate(platformPrefab, platformPos, this.gameObject.transform.rotation, this.gameObject.transform); //make a platform at the platformPos
+            //update decaytime so new blocks generate not in blocks
+            block.GetComponent<EndlessPlatform>().decayTime += i * 2;
         }
+    }
+
+    public GameObject GeneratePlatform()
+    {
+        float gapLeft = maxGap;
+
+        float addZ = Random.Range(0, zVariation); //only go forward
+        gapLeft-= addZ;
+
+        float addX = Mathf.Clamp(Random.Range(-xVariation, xVariation), -gapLeft, gapLeft); //left/right
+        gapLeft -= Mathf.Abs(addX);
+
+
+        float addY = Random.Range(-yVariation, yVariation); //up/down
+
+        platformPos = new Vector3((platformPos.x + addX), (platformPos.y + addY), (platformPos.z + addZ));
+        GameObject block = GameObject.Instantiate(platformPrefab, platformPos, this.gameObject.transform.rotation, this.gameObject.transform); //make a platform at the platformPos
+
+        return block;
     }
 }
