a9c4ac01924b7591d0bbca6e92b3e52e055d91f4
Joel Shuart <joelshuart@gmail.com>
2018-11-14 23:44:56 -0500

    rebuilt endless generation to resolve errors and for optimization

--- TheFloorIsLava/Assets/Scripts/EndlessManager.cs
+++ TheFloorIsLava/Assets/Scripts/EndlessManager.cs
@@ -8,14 +8,20 @@ public class EndlessManager : MonoBehaviour {
     [SerializeField] private float yVariation;
     [SerializeField] private float zVariation;
     [SerializeField] private float maxGap;
+
     [SerializeField] private int numOfPlatforms;
+    [SerializeField] private List<GameObject> platforms;
+    private int platformToMove = -1;
+
     [SerializeField] private GameObject platformPrefab;
-    private List<GameObject> platforms;
-    Vector3 platformPos;
+    private Vector3 platformPos;
+
+    [SerializeField] private float decayTime;
 
 	// Use this for initialization
 	void Start () {
         BuildInital();
+        StartCoroutine(MoveNextAfterSeconds());
 		
 	}
 	
@@ -30,29 +36,50 @@ public class EndlessManager : MonoBehaviour {
         platformPos = this.gameObject.transform.position; //start from root
         for (int i = 0; i < numOfPlatforms; i++)
         {
-            GameObject block = GeneratePlatform();
+            //create a platrom and add it to our list
+            GameObject block = GameObject.Instantiate(platformPrefab, platformPos, this.gameObject.transform.rotation, this.gameObject.transform); //make a platform at the platformPos
+            platforms.Add(block);
 
-            //update decaytime so new blocks generate not in blocks
-            block.GetComponent<EndlessPlatform>().decayTime += i * 2;
+            //move platfomr to proper spot
+            MovePlatform(block);
         }
+
+        //platformToMove = numOfPlatforms;
     }
 
-    public GameObject GeneratePlatform()
+    private void MovePlatform(GameObject platform)
     {
+        //set amount of gap
         float gapLeft = maxGap;
 
+        //give variance to xyz positions - and count down amount of gap used
         float addZ = Random.Range(0, zVariation); //only go forward
         gapLeft-= addZ;
 
         float addX = Mathf.Clamp(Random.Range(-xVariation, xVariation), -gapLeft, gapLeft); //left/right
         gapLeft -= Mathf.Abs(addX);
 
-
         float addY = Random.Range(-yVariation, yVariation); //up/down
 
+        //create new platform position
         platformPos = new Vector3((platformPos.x + addX), (platformPos.y + addY), (platformPos.z + addZ));
-        GameObject block = GameObject.Instantiate(platformPrefab, platformPos, this.gameObject.transform.rotation, this.gameObject.transform); //make a platform at the platformPos
 
-        return block;
+        //move defined obj to newly defined pos
+        platform.transform.position = platformPos;
+    }
+
+    IEnumerator MoveNextAfterSeconds()
+    {
+        //wait for the deccay time
+        yield return new WaitForSeconds(decayTime);
+
+        //change platform index
+        platformToMove = ((platformToMove + 1) % platforms.Count);
+
+        //move platform
+        MovePlatform(platforms[platformToMove]);
+
+        //recursive- start another run
+        StartCoroutine(MoveNextAfterSeconds());
     }
 }
